0225ca47afac259e4bed93609bdc2852aee74a8f
Israel Campos Ruiz <israelcamp12@gmail.com>
2024-12-05 11:45:54 -0600

    implement baseRepository abstract class for the contract signature

--- src/pokemons/infraestructure/repositorys/Base.repository.ts
+++ src/pokemons/infraestructure/repositorys/Base.repository.ts
@@ -1,6 +1,4 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-export class BaseRepository<T> {
-  create(entity: T): Promise<T> {
-    throw new Error('method not implemented');
-  }
+export abstract class BaseRepository<T> {
+  abstract create(_entity: T): Promise<T>;
 }
